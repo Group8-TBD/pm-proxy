11b7bf0b3147cc9b65d9390008300b94b8de3580
Patrick Mac <patmac510@Patricks-Air.attlocal.net>
2020-04-17 16:08:05 -0700

    updated to new links and api routes

--- server.js
+++ server.js
@@ -1,51 +1,44 @@
 const express = require("express");
-const app = express();
-const port = 3030;
+const port = 3000;
 const path = require("path");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require("cors");
-const httpProxy = require("http-proxy");
 
-const proxy = httpProxy.createProxyServer({});
+const app = express();
+
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/api/properties', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://ec2-13-59-243-8.us-east-2.compute.amazonaws.com/"
-      });
-} )
+const photos = 'http://ec2-54-153-107-63.us-west-1.compute.amazonaws.com:3001/';
+const reservations = "http://3.21.207.217:3111/";
+const recommendations ='http://ec2-54-241-236-42.us-west-1.compute.amazonaws.com/';
+const reviews = "http://ec2-54-183-203-80.us-west-1.compute.amazonaws.com:3500/"
 
-app.all('/images*', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://ec2-13-59-243-8.us-east-2.compute.amazonaws.com/"
-      });
-} )
+//photos
+app.all('/properties/:property_id', createProxyMiddleware({ target: photos}));
+app.all('/images/image_id', createProxyMiddleware({ target: photos}));
+app.all('/images', createProxyMiddleware({ target: photos}));
+app.all('/properties/:property_name', createProxyMiddleware({ target: photos}));
 
+//reservations
+app.all('/api/reservations/:roomId', createProxyMiddleware({ target: reservations}));
+app.all('/api/reservations/', createProxyMiddleware({ target: reservations}));
+app.all('/api/reservations/:reservationId', createProxyMiddleware({ target: reservations}));
 
-app.all('/api/listing', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://localhost:3500"
-      });
-} )
+//reviews
+app.all('/api/listing/:listingID', createProxyMiddleware({ target: reviews}));
+app.all('/api/listing/', createProxyMiddleware({ target: reviews}));
+app.all('api/createReview', createProxyMiddleware({ target: reviews}));
+app.all('api/createListing', createProxyMiddleware({ target: reviews}));
+app.all('api/updateListing', createProxyMiddleware({ target: reviews}));
+app.all('api/deleteListing', createProxyMiddleware({ target: reviews}));
 
-app.all('/api/reviews', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://localhost:3500"
-      });
-} )
-
-app.all('/recommendations', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://ec2-3-89-251-90.compute-1.amazonaws.com:3009/"
-      });
-} )
-
-app.all('/photos', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://localhost:3001"
-      });
-} )
+//recommendations
+app.all('/recommendations/:zip', createProxyMiddleware({ target: recommendations}));
+app.all('/images/listing/:id', createProxyMiddleware({ target: recommendations}));
+app.all('/recommendations', createProxyMiddleware({ target: recommendations}));
+app.all('/recommendations/listing/:id', createProxyMiddleware({ target: recommendations}));
 
 
 app.listen(port, () => console.log(`Proxy server running on port ${port}`));
